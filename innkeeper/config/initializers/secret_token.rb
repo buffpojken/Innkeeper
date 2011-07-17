058b9e04652b0d53336043679dfbea5d216cdd6b
buffpojken <daniel@sykewarrior.com>
2011-07-17 19:55:18 +0200

    initial commit

---
+++ innkeeper/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innkeeper::Application.config.secret_token = '3cf33f731c5351c9ea05d9d93dfa4d34ff43a86c34b3bbe914f8455fa107482b9e8da415c63454455edd928f8693b4e215c3382ae3b336d1af064b35b6d7ab4e'
